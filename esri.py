5f7b65fcebab1e003dfc695c2179548f59f11a95
Peter Yoon <peterdyoon@gmail.com>
2019-08-13 10:56:06 -0500

    break out get_value and get_df as well as abs path for config file

--- esri.py
+++ esri.py
@@ -2,7 +2,8 @@ import requests, json, yaml
 import pandas as pd
 
 def get_token():
-	account_info = yaml.load(open("esri_account_info.yaml", "r"))
+
+	account_info = yaml.load(open("/Users/peteryoon/Documents/Esri/esri_account_info.yaml", "r"))
 
 	company_url = "https://msidecap.maps.arcgis.com/"
 	url = "https://www.arcgis.com/sharing/generateToken?parameters"
@@ -17,13 +18,13 @@ def get_token():
 	response = requests.post(url=url, params=params)
 	token = response.content.split('"token":"')[1].split('","')[0]
 
-	with open("access_token.yaml", "w") as outfile:
+	with open("/Users/peteryoon/Documents/Esri/access_token.yaml", "w") as outfile:
 		yaml.safe_dump(token, outfile)
 
 
-def get_df(location, rings=[1, 3, 5], data_type=["KeyUSFacts.TOTPOP_CY", "KeyUSFacts.MEDHINC_CY"]):
+def get_response(location, rings, data_types):
 
-	token = yaml.load(open("access_token.yaml", "r"))
+	token = yaml.load(open("/Users/peteryoon/Documents/Esri/access_token.yaml", "r"))
 
 	if isinstance(location, str):
 		area_details = {
@@ -39,7 +40,7 @@ def get_df(location, rings=[1, 3, 5], data_type=["KeyUSFacts.TOTPOP_CY", "KeyUSF
 			"bufferUnits": "esriMiles",
 			"bufferRadii": rings
 		}		
-	url = 'https://geoenrich.arcgis.com/arcgis/rest/services/World/geoenrichmentserver/GeoEnrichment/enrich?studyAreas=[{}]&analysisVariables={}'.format(str(area_details), str(data_type))
+	url = 'https://geoenrich.arcgis.com/arcgis/rest/services/World/geoenrichmentserver/GeoEnrichment/enrich?studyAreas=[{}]&analysisVariables={}'.format(str(area_details), str(data_types))
 	headers = {"Authorization": token}
 	params = {
 	    "token": token,
@@ -49,6 +50,7 @@ def get_df(location, rings=[1, 3, 5], data_type=["KeyUSFacts.TOTPOP_CY", "KeyUSF
 	response = requests.post(url=url, params=params)
 
 	response_json = json.loads(response.content)
+
 	try:
 		field_names = response_json["results"][0]["value"]["FeatureSet"][0]["fieldAliases"]
 	except:
@@ -60,13 +62,58 @@ def get_df(location, rings=[1, 3, 5], data_type=["KeyUSFacts.TOTPOP_CY", "KeyUSF
 				raise SyntaxError("Invalid token; generating new token. Try again.")
 		else:
 			raise ValueError("Unknown error: See json blob. >> {}".format(response_json))
+
+	return response_json
+
+
+def get_df(location, rings=[1, 2, 3], data_types=["KeyUSFacts.TOTPOP_CY", "KeyUSFacts.MEDHINC_CY"]):
+
+	response_json = get_response(location, rings, data_types)
+
 	values_per_ring = [value["attributes"] for value in response_json["results"][0]["value"]["FeatureSet"][0]["features"]]
 
+	field_names = response_json["results"][0]["value"]["FeatureSet"][0]["fieldAliases"]
 	final = {"Field Name":[field_names[key] for key in values_per_ring[0].keys()], "Internal Name": [key for key in values_per_ring[0].keys()]}
 	for index, ring_value in enumerate(values_per_ring):
 		final["Mile {}".format(ring_value["bufferRadii"])] = [value for value in ring_value.values()]
 
 	print "ESRI Search Complete for {}".format(location)
 	df = pd.DataFrame(final)
-	short_data_types = [data.split(".")[-1] for data in data_type]
-	return df[df["Internal Name"].str.contains("|".join(short_data_types))]
+
+	if isinstance(data_types, list):
+		short_data_types = [data.split(".")[-1] for data in data_types]
+		return df[df["Internal Name"].str.contains("|".join(short_data_types))]
+	elif isinstance(data_types, str):
+		short_data_types = data_types.split(".")[-1]
+		return df[df["Internal Name"] == short_data_types]
+	else:
+		raise SyntaxError("Make sure data types are correct.")
+	
+
+
+def get_value(location, ring=1, data_type="KeyUSFacts.TOTPOP_CY"):
+
+	if isinstance(ring, list) or isinstance(data_type, list):
+		raise SyntaxError("Ring or data type is a list; remove brackets --> []")
+
+	response_json = get_response(location, [ring], [data_type])
+
+	short_data_type = data_type.split(".")[-1]
+
+	print response_json["results"][0]["value"]["FeatureSet"][0]["features"][0]["attributes"][short_data_type]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
